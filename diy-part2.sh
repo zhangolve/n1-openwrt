946869c3430d8ca5d70927b560edb8e86d5c3584
zhangolve <zhangolve@gmail.com>
2021-09-29 15:56:57 +0800

    update config

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,22 +1,22 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+# #!/bin/bash
+# #
+# # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# #
+# # This is free software, licensed under the MIT License.
+# # See /LICENSE for more information.
+# #
+# # https://github.com/P3TERX/Actions-OpenWrt
+# # File name: diy-part2.sh
+# # Description: OpenWrt DIY script part 2 (After Update feeds)
+# #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-mkdir package/luci-app-openclash
-cd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull origin master
-git branch --set-upstream-to=origin/master master
+# # Modify default IP
+# #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# mkdir package/luci-app-openclash
+# cd package/luci-app-openclash
+# git init
+# git remote add -f origin https://github.com/vernesong/OpenClash.git
+# git config core.sparsecheckout true
+# echo "luci-app-openclash" >> .git/info/sparse-checkout
+# git pull origin master
+# git branch --set-upstream-to=origin/master master
