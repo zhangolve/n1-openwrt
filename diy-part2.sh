f501e51f4c0931148b2b19f8d1cb466b1cd08281
zhangolve <zhangolve@users.noreply.github.com>
2022-06-04 11:03:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,11 +12,11 @@
 
 # # Modify default IP
 # #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-# mkdir package/luci-app-openclash
-# cd package/luci-app-openclash
-# git init
-# git remote add -f origin https://github.com/vernesong/OpenClash.git
-# git config core.sparsecheckout true
-# echo "luci-app-openclash" >> .git/info/sparse-checkout
-# git pull origin master
-# git branch --set-upstream-to=origin/master master
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
